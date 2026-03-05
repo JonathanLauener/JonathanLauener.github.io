d039fb6f288a911c5d43fb4d1783bae1e32bb326
JonathanLauener <selfimprovementjonathan@gmail.com>
2026-03-05 06:13:50 +0100

    Update main.js

--- main.js
+++ main.js
@@ -60,7 +60,7 @@ document.addEventListener("DOMContentLoaded", function () {
         term.writeln(line);
       }
     });
-    renderStatus("-- NORMAL --\n");
+    renderStatus("\n-- NORMAL --\n");
   }
 
   function renderStatus(text) {
